40bdeb345ae3d5649c934019bb0ba8d23ee87f40
Ronald Rajski <ronald.rajski@gmail.com>
2020-02-29 08:58:36 -0600

    Week 4

--- 1-HTML-CSS-JavaScript/Week4-AddRows.js
+++ 1-HTML-CSS-JavaScript/Week4-AddRows.js
@@ -1,12 +1,20 @@
-function insert_Row(){
-    var x=document.getElementById('sampleTable').insertRow(0);
-    var y = x.insertCell(0);
-    var z = x.insertCell(1);
-    y.innerHTML="New Cell1";
-    z.innerHTML="New Cell2";
+// function insert_list(){
+//     var x=document.getElementById('sampleTable').insert_list(0);
+//     var y = x.insertCell(0);
+//     var z = x.insertCell(1);
+//     y.innerHTML="New Cell1";
+//     z.innerHTML="New Cell2";
 
+// }
+
+function addItem(){
+    var ol = document.getElementById("dynamic-list");
+    var candidate = document.getElementById("candidate");
+    var li = document.createElement("li");
+    li.setAttribute('id',candidate.value);
+    li.appendChild(document.createTextNode(candidate.value));
+    ol.appendChild(li);
 }
-
 
 
 
